f4e2a8deffbfb3b608204315d2f1a9b4880a27f1
Jakov-Markic <jmarki00@fesb.hr>
2026-03-25 15:56:28 +0100

    Prvi commit

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Nikad na lazio");
             Console.WriteLine("Hello World!");
         }
     }
